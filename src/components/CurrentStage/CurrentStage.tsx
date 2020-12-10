8f7962eeab56a68a55d00fd01eac5c32a9912992
Matteias Collet <matteias.collet@bluewin.ch>
2020-12-10 13:28:51 +0100

    minor style fixes

--- src/components/CurrentStage/CurrentStage.tsx
+++ src/components/CurrentStage/CurrentStage.tsx
@@ -5,11 +5,12 @@ import styles from './CurrentStage.module.css';
 
 type Props = {
   price: number;
+  className?: string;
 };
 
-const CurrentStage = ({ price }: Props) => {
+const CurrentStage = ({ className, price }: Props) => {
   return (
-    <div className={styles.wrapper}>
+    <div className={cx(styles.wrapper, className)}>
       <FormattedPrice price={price} />
     </div>
   );
